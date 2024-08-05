f292e160912f01c854e7a113c8c3cbc7b8759edc
Haji Mohammed <hajisky07@gmail.com>
2024-08-05 10:02:31 +0100

    chore: Update Nuxt configuration and ShowcaseList component

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,92 +1,88 @@
 <template>
-    <div class="w-full py-20 sm:py-32">
-        <UContainer class="grid sm:grid-cols-3 gap-10 sm:gap-4 sm:items-center">
-            <div class="flex flex-col gap-10">
-                <div class="flex items-center gap-2">
-                    <NuxtImg
-                        src="/pet-1.jpeg"
-                        alt="pet 1"
-                        width="50px"
-                        height="50px"
-                        format="webp"
-                    />
-                    <NuxtImg
-                        src="/pet-2.jpeg"
-                        alt="pet 2"
-                        width="50px"
-                        height="50px"
-                        format="webp"
-                    />
-                </div>
+  <div class="w-full py-20 sm:py-32">
+    <UContainer class="grid sm:grid-cols-3 gap-10 sm:gap-4 sm:items-center">
+      <div class="flex flex-col gap-10">
+        <div class="flex items-center gap-2">
+          <img
+            src="/pet-1.jpeg"
+            alt="pet 1"
+            width="50px"
+            height="50px"
+            format="webp"
+          />
+          <img
+            src="/pet-2.jpeg"
+            alt="pet 2"
+            width="50px"
+            height="50px"
+            format="webp"
+          />
+        </div>
 
-                <h1 class="text-3xl font-semibold sm:text-5xl">
-                    I'm a <br />
-                    <span class="text-primary-300">UI/UX Designer</span>
-                </h1>
+        <h1 class="text-3xl font-semibold sm:text-5xl">
+          I'm a <br />
+          <span class="text-primary-300">UI/UX Designer</span>
+        </h1>
 
-                <div class="hidden sm:flex items-center gap-8">
-                    <div class="flex flex-col gap-2">
-                        <span class="text-gray-400 text-xs uppercase"
-                            >Years of experience</span
-                        >
-                        <span class="text-2xl font-semibold">3+</span>
-                    </div>
-                    <div class="flex flex-col gap-2">
-                        <span class="text-gray-400 text-xs uppercase"
-                            >Project Done</span
-                        >
-                        <span class="text-2xl font-semibold">40+</span>
-                    </div>
-                </div>
-            </div>
+        <div class="hidden sm:flex items-center gap-8">
+          <div class="flex flex-col gap-2">
+            <span class="text-gray-400 text-xs uppercase"
+              >Years of experience</span
+            >
+            <span class="text-2xl font-semibold">3+</span>
+          </div>
+          <div class="flex flex-col gap-2">
+            <span class="text-gray-400 text-xs uppercase">Project Done</span>
+            <span class="text-2xl font-semibold">40+</span>
+          </div>
+        </div>
+      </div>
 
-            <div class="flex flex-col gap-10">
-                <NuxtImg
-                    src="/hero-image.jpeg"
-                    alt="Hero Image"
-                    class="max-w-[280px] sm:max-w-xs mx-auto"
-                />
+      <div class="flex flex-col gap-10">
+        <img
+          src="/hero-image.jpeg"
+          alt="Hero Image"
+          class="max-w-[280px] sm:max-w-xs mx-auto"
+        />
 
-                <div class="flex justify-between gap-10 items-center sm:hidden">
-                    <div class="flex flex-col gap-2">
-                        <span class="text-gray-400 text-xs uppercase"
-                            >Years of experience</span
-                        >
-                        <span class="text-2xl font-semibold">3+</span>
-                    </div>
-                    <div class="flex flex-col gap-2">
-                        <span class="text-gray-400 text-xs uppercase"
-                            >Project Done</span
-                        >
-                        <span class="text-2xl font-semibold">40+</span>
-                    </div>
-                </div>
-            </div>
+        <div class="flex justify-between gap-10 items-center sm:hidden">
+          <div class="flex flex-col gap-2">
+            <span class="text-gray-400 text-xs uppercase"
+              >Years of experience</span
+            >
+            <span class="text-2xl font-semibold">3+</span>
+          </div>
+          <div class="flex flex-col gap-2">
+            <span class="text-gray-400 text-xs uppercase">Project Done</span>
+            <span class="text-2xl font-semibold">40+</span>
+          </div>
+        </div>
+      </div>
 
-            <div class="sm:ml-auto flex flex-col w-full max-w-[250px] gap-6">
-                <div class="flex flex-col gap-2">
-                    <span class="text-gray-400 uppercase">BASED IN</span>
-                    <p class="font-medium">Leeds</p>
-                </div>
-                <div class="flex flex-col gap-2">
-                    <span class="text-gray-400 uppercase">BIOGRAPHY</span>
-                    <p class="font-medium">Passionate about beautiful things</p>
-                    <p>
-                        Inspired by everyday moments, I strive to craft visually
-                        compelling and user-friendly experiences.
-                    </p>
-                </div>
+      <div class="sm:ml-auto flex flex-col w-full max-w-[250px] gap-6">
+        <div class="flex flex-col gap-2">
+          <span class="text-gray-400 uppercase">BASED IN</span>
+          <p class="font-medium">Leeds</p>
+        </div>
+        <div class="flex flex-col gap-2">
+          <span class="text-gray-400 uppercase">BIOGRAPHY</span>
+          <p class="font-medium">Passionate about beautiful things</p>
+          <p>
+            Inspired by everyday moments, I strive to craft visually compelling
+            and user-friendly experiences.
+          </p>
+        </div>
 
-                <div class="flex flex-col gap-2">
-                    <span class="text-gray-400 uppercase">SKILLS</span>
-                    <p class="font-medium">UI/UX (Figma & Sketch)</p>
-                    <p class="font-medium">Digital Markteting</p>
-                    <p class="font-medium">Adobe Suite (AI,XD,PS)</p>
-                    <p class="font-medium">HTML</p>
-                </div>
-            </div>
-        </UContainer>
-    </div>
+        <div class="flex flex-col gap-2">
+          <span class="text-gray-400 uppercase">SKILLS</span>
+          <p class="font-medium">UI/UX (Figma & Sketch)</p>
+          <p class="font-medium">Digital Markteting</p>
+          <p class="font-medium">Adobe Suite (AI,XD,PS)</p>
+          <p class="font-medium">HTML</p>
+        </div>
+      </div>
+    </UContainer>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
   ssr: false,
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/content", "@nuxt/image", "@nuxt/fonts"],
+  modules: ['@nuxt/ui', '@nuxt/content', '@nuxt/image', '@nuxt/fonts'],
+
   app: {
     head: {
       htmlAttrs: {
-        class: "scroll-smooth",
+        class: 'scroll-smooth',
       },
     },
   },
+
   router: {
     options: {
-      scrollBehaviorType: "smooth",
+      scrollBehaviorType: 'smooth',
     },
   },
+
+  compatibilityDate: '2024-08-05',
 })
